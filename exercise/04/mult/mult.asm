fe9b7713f35d2124b6a89c74ac3f1eab32537462
HH <howard19970613.gmail.com>
2018-12-20 14:35:13 +0800

    final exam

--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -7,3 +7,24 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @0
+    D=A//拿0位置上的數值放在D上
+    @2
+    M=D
+
+(loop)
+    @0
+    D=M
+    @exit
+    D;JLE
+    @1
+    D=M
+    @2
+    M=D+M
+    @0
+    M=M-1
+    @loop
+    0,JMP
+(exit)
+    @exit
+    0;JMP
